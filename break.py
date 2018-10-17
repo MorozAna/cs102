da678143feb5625a4d9b75c4fe4cb13bb9870008
Moroz Anastasiia <43277899+MorozAna@users.noreply.github.com>
2018-10-18 01:46:02 +0400

    .

--- break.py
+++
@@ -1,6 +0,0 @@
-while True:
-	s = input('Insert smth: ')
-	if s == 'Quit':
-		break
-	print('Lenght:', len(s))
-print('Finished.')
--- continue.py
+++
@@ -1,8 +0,0 @@
-while True:
-	s = input("Insert smth: ")
-	if s == "Quit":
-		break
-	if len(s) < 3:
-		print('More')
-		continue
-	print("That's enough")
