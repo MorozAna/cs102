4cbcdebd79ac2548c264e0020aa39fed741274fd
Moroz Anastasiia <43277899+MorozAna@users.noreply.github.com>
2018-10-18 01:36:42 +0400

    Добавлен шаблон, реализованы обе функции для шифра Виженера, код проверен на pep8

--- homework01/vigener.py
+++ homework01/vigener.py
@@ -1,3 +1,39 @@
+def encrypt_vigenere(plaintext, keyword):
+    """
+    >>> encrypt_vigenere("PYTHON", "A")
+    'PYTHON'
+    >>> encrypt_vigenere("python", "a")
+    'python'
+    >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
+    'LXFOPVEFRNHR'
+    """
+    # PUT YOUR CODE HERE
+    ciphertext = ""
+    i = 0
+    for char in plaintext:
+        if "a" <= char <= "z" or "A" <= char <= "Z":
+            shift = ord(keyword[i])
+            if "a" <= char <= "z":
+                shift -= ord("a")
+                if shift <= ord("z") - ord(char):
+                    ciphertext += chr(ord(char) + shift)
+                else:
+                    ciphertext += chr(ord(char) - 26 + shift)
+            else:
+                shift -= ord("A")
+                if shift <= ord("Z") - ord(char):
+                    ciphertext += chr(ord(char) + shift)
+                else:
+                    ciphertext += chr(ord(char) - 26 + shift)
+        else:
+            ciphertext += char
+        if len(keyword) == i + 1:
+            i = 0
+        else:
+            i += 1
+    return ciphertext
+
+
 def decrypt_vigenere(ciphertext, keyword):
     """
     >>> decrypt_vigenere("PYTHON", "A")
