eb6e726b2d9e148eca10fafa8ef4d96e86c3922c
Moroz Anastasiia <43277899+MorozAna@users.noreply.github.com>
2018-11-12 15:36:03 +0400

    Исправлена табуляция

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -26,16 +26,16 @@ def group(values, n):
     [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     """
     groups = []
-	a = []
-	count = len(values) // n
-	i = 0
-	for j in range(count):
-		while len(a) < n:
-			a.append(values[i])
-			i += 1
-		groups.append(a)
-		a = []
-	return groups
+    a = []
+    count = len(values) // n
+    i = 0
+    for j in range(count):
+        while len(a) < n:
+            a.append(values[i])
+            i += 1
+        groups.append(a)
+        a = []
+    return groups
 
 
 def get_row(values, pos):
