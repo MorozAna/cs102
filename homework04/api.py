73dc34482d47eed80a3ced4f19cf38b6c19fe006
Moroz Anastasiia <43277899+MorozAna@users.noreply.github.com>
2018-12-18 15:42:30 +0400

    Добавлены функции

--- homework04/api.py
+++ homework04/api.py
@@ -13,7 +13,18 @@ def get(url, params={}, timeout=5, max_retries=5, backoff_factor=0.3):
     :param max_retries: максимальное число повторных запросов
     :param backoff_factor: коэффициент экспоненциального нарастания задержки
     """
-    # PUT YOUR CODE HERE
+    for n in range(max_retries):
+        try:
+            response = requests.get(query, params=params, timeout=timeout)
+            content_type = response.headers.get('Content-Type')
+            if not content_type == "application/json; charset=utf-8":
+                raise
+            return response
+        except requests.exceptions.RequestException:
+            if n == max_retries - 1:
+                raise
+            backoff_value = backoff_factor * (2 ** n)
+            time.sleep(backoff_value)
 
 
 def get_friends(user_id, fields):
@@ -25,7 +36,20 @@ def get_friends(user_id, fields):
     assert isinstance(user_id, int), "user_id must be positive integer"
     assert isinstance(fields, str), "fields must be string"
     assert user_id > 0, "user_id must be positive integer"
-    # PUT YOUR CODE HERE
+    domain = "https://api.vk.com/method"
+    access_token = ''
+    user_id = user_id
+
+    query_params = {
+        'domain' : domain,
+        'access_token': access_token,
+        'user_id': user_id,
+        'fields': 'sex'
+    }
+
+    query = "{domain}/friends.get?access_token={access_token}&user_id={user_id}&fields={fields}&v=5.53".format(**query_params)
+    response = requests.get(query)
+    return response.json()
 
 
 def messages_get_history(user_id, offset=0, count=20):
@@ -40,4 +64,16 @@ def messages_get_history(user_id, offset=0, count=20):
     assert isinstance(offset, int), "offset must be positive integer"
     assert offset >= 0, "user_id must be positive integer"
     assert count >= 0, "user_id must be positive integer"
-    # PUT YOUR CODE HERE
+    domain = "https://api.vk.com/method"
+    access_token = VK_CONFIG['access_token']
+    user_id = user_id
+
+    query_params = {
+        'domain' : domain,
+        'access_token': access_token,
+        'user_id': user_id,
+    }
+
+    query = "{domain}/messages.getHistory?access_token={access_token}&user_id={user_id}&fields={fields}&v=5.53".format(**query_params)
+    response = requests.get(query)
+    return response.json()
