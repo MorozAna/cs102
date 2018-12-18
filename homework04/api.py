6d5a7bf47a2482c4087160ecf2ae2f2fae6f1505
Moroz Anastasiia <43277899+MorozAna@users.noreply.github.com>
2018-12-18 15:52:44 +0400

    Fields

--- homework04/api.py
+++ homework04/api.py
@@ -39,12 +39,13 @@ def get_friends(user_id, fields):
     domain = "https://api.vk.com/method"
     access_token = ''
     user_id = user_id
+    fields = fields
 
     query_params = {
         'domain' : domain,
         'access_token': access_token,
         'user_id': user_id,
-        'fields': 'sex'
+        'fields': fields
     }
 
     query = "{domain}/friends.get?access_token={access_token}&user_id={user_id}&fields={fields}&v=5.53".format(**query_params)
